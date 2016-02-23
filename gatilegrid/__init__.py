1bbc8edca8605a52bee8732b657e9e250185c419
Gasser Loïc <loicgasser4@gmail.com>
2016-02-23 17:00:45 +0100

    Initial commit

---
+++ gatilegrid/__init__.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+
+import math
+
+
+class GeoadminTileGrid:
+
+    # Defines zooms 0 to 27
+    RESOLUTIONS = [
+        4000.0,
+        3750.0,
+        3500.0,
+        3250.0,
+        3000.0,
+        2750.0,
+        2500.0,
+        2250.0,
+        2000.0,
+        1750.0,
+        1500.0,
+        1250.0,
+        1000.0,
+        750.0,
+        650.0,
+        500.0,
+        250.0,
+        100.0,
+        50.0,
+        20.0,
+        10.0,
+        5.0,
+        2.5,
+        2.0,
+        1.5,
+        1.0,
+        0.5,
+        0.25,
+        0.1
+    ]
+
+    # Default Swiss extent
+    MINX = 420000.0
+    MAXX = 900000.0
+    MINY = 30000.0
+    MAXY = 350000.0
+
+    XSPAN = MAXX - MINX
+    YSPAN = MAXY - MINY
+
+    spatialReference = 21781
+
+    def __init__(self, extent=None, tileSizePx=256.0):
+        if extent:
+            assert extent[0] < extent[2]
+            assert extent[1] < extent[3]
+            assert extent[0] >= self.MINX
+            assert extent[1] >= self.MINY
+            assert extent[2] <= self.MAXX
+            assert extent[3] <= self.MAXY
+            self.extent = extent 
+        else:
+            self.extent = [self.MINX, self.MINY, self.MAXX, self.MAXY]
+        self.origin = [self.extent[0], self.extent[3]]  # Top left corner
+        self.tileSizePx = tileSizePx  # In pixels
+        self.tileAddressTemplate = '{zoom}/{tileCol}/{tileRow}'
+    
+    def tileSize(self, zoom):
+        "Returns the size (in meters) of a tile" 
+        assert zoom in range(0, len(self.RESOLUTIONS))
+        return self.tileSizePx * self.RESOLUTIONS[int(zoom)]
+
+    def tileBounds(self, zoom, tileCol, tileRow):
+        "Returns the bounds of a tile in LV03 (EPSG:21781)"
+        assert zoom in range(0, len(self.RESOLUTIONS))
+        minX = self.MINX + (tileCol * self.tileSize(zoom))
+        minY = self.MAXY - ((tileRow + 1) * self.tileSize(zoom))
+        maxX = self.MINX + ((tileCol + 1) * self.tileSize(zoom))
+        maxY = self.MAXY - (tileRow * self.tileSize(zoom))
+        return [minX, minY, maxX, maxY]
+
+    def tileAddress(self, zoom, topLeft):
+        "Returns a tile address based on a zoom level and \
+        the top left coordinate of a tile"
+        [x, y] = topLeft
+        assert x <= self.MAXX and x >= self.MINX
+        assert y <= self.MAXY and y >= self.MINY
+        assert zoom in range(0, len(self.RESOLUTIONS))
+
+        tileS = self.tileSize(zoom)
+        offsetX = self.XSPAN - (self.MAXX - x)
+        offsetY = self.YSPAN - (y - self.MINY)
+        totNbTilesX = math.ceil(self.XSPAN / self.tileSize(zoom))
+        totNbTilesY = math.ceil(self.YSPAN / self.tileSize(zoom))
+        return [
+            int(round((offsetX / self.XSPAN) * totNbTilesX)),  # Row
+            int(round((offsetY / self.YSPAN) * totNbTilesY))  # Col
+        ]
+
+    def iterGrid(self, minZoom, maxZoom):
+        "Yields the tileBounds, zoom, tileCol and tileRow"
+        assert minZoom in range(0, len(self.RESOLUTIONS))
+        assert maxZoom in range(0, len(self.RESOLUTIONS))
+        assert minZoom <= maxZoom
+
+        for zoom in xrange(minZoom, maxZoom + 1):
+            maxX = self.extent[2]
+            minY = self.extent[1]
+            [tileRow, tileCol] = self.tileAddress(zoom, self.origin)
+            while self.extent[2] >= maxX:
+                while self.extent[1] <= minY:
+                    tileBounds = self.tileBounds(zoom, tileCol, tileRow)
+                    [minX, minY, maxX, maxY] = tileBounds
+                    yield (tileBounds, zoom, tileCol, tileRow)
+                    tileRow += 1
+                minY = self.extent[1]
+                tileCol += 1
+                tileRow = 0
+
+    def numberOfXTilesAtZoom(self, zoom):
+        "Returns the number of tiles over x at a given zoom level"
+        return math.ceil(self.xSpan / self.tileSize(zoom))
+
+    def numberOfYTilesAtZoom(self, zoom):
+        "Retrurns the number of tiles over y at a given zoom level"
+        return math.ceil(self.ySpan / self.tileSize(zoom))
+
+    def numberOfTilesAtZoom(self, zoom):
+        "Returns the total number of tile at a given zoom level"
+        return self.numberOfXTilesAtZoom(zoom) * self.numberOfYTilesAtZoom(zoom)
+
+    def getResolution(self, zoom):
+        "Return the image resolution at a given zoom level"
+        return self.tileSize(zoom) / self.tileSizePx
+
+    def getScale(self, zoom, dpi=96.0):
+        "Return the scale at a given zoom level \
+        (1:x e.g. 1 map unit equal x unit in the real world)"
+        inchesPerMeters = 39.37
+        return self.getResolution(zoom) * inchesPerMeters * dpi
+
+    @property
+    def xSpan(self):
+        "Returns the range in meters over x"
+        if self.extent:
+            return self.extent[2] - self.extent[0]
+        return self.XSPAN
+
+    @property
+    def ySpan(self):
+        "Returns the range in meters over y"
+        if self.extent:
+            return self.extent[3] - self.extent[1]
+        return self.YSPAN
---
+++ tests/test_gatilegrid.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from gatilegrid import GeoadminTileGrid
+
+
+class TestGeoadminTileGrid(unittest.TestCase):
+
+    def testTileGridWrongExtent(self):
+        try:
+            GeoadminTileGrid(extent=[10.0, 10.0, 20.0, 20.0])
+        except Exception as e:
+            self.assertIsInstance(e, AssertionError)
+        else:
+            raise Exception('GeoadminTileGrid instance: extent assertion error \
+                too small not raised')
+
+        try:
+            GeoadminTileGrid(extent=[430000.0, 40000.0, 420000.0, 340000.0])
+        except Exception as e:
+            self.assertIsInstance(e, AssertionError)
+        else:
+            raise Exception('GeoadminTileGrid instance: extent assertion error \
+                inconsistent not raised')
+
+    def testTileSize(self):
+        gagrid = GeoadminTileGrid()
+        ts = gagrid.tileSize(20)
+        self.assertEqual(ts, 2560.0)
+        try:
+            gagrid.tileSize(40)
+        except Exception as e:
+            self.assertIsInstance(e, AssertionError)
+        else:
+            raise Exception('tileSize: assertion error not raised')
+
+        self.assertEqual(gagrid.tileAddressTemplate,
+            '{zoom}/{tileCol}/{tileRow}')
+
+    def testTileBoundsAndAddress(self):
+        gagrid = GeoadminTileGrid()
+        tbe = [548000.0, 196400.0, 573600.0, 222000.0]
+        tb = gagrid.tileBounds(17, 5, 5)
+        self.assertEqual(tb[0], tbe[0])
+        self.assertEqual(tb[1], tbe[1])
+        self.assertEqual(tb[2], tbe[2])
+        self.assertEqual(tb[3], tbe[3])
+        try:
+            gagrid.tileBounds(77, 5, 5)
+        except Exception as e:
+            self.assertIsInstance(e, AssertionError)
+        else:
+            raise Exception('tileBounds: assertion error not raised') 
+
+        ta = gagrid.tileAddress(0, [gagrid.MINX, gagrid.MAXY])
+        self.assertEqual(ta[0], 0)
+        self.assertEqual(ta[1], 0)
+        ta = gagrid.tileAddress(17, [tb[0], tb[3]])
+        self.assertEqual(ta[0], 5)
+        self.assertEqual(ta[1], 5)
+
+    def testIterGrid(self):
+        gagrid = GeoadminTileGrid()
+        gen = gagrid.iterGrid(0, 0)
+        self.assertTrue(hasattr(gen, '__iter__'))
+        tileSpec = [t for t in gen]
+        self.assertEqual(len(tileSpec), 1)
+        self.assertEqual(len(tileSpec[0]), 4)
+        self.assertEqual(tileSpec[0][1], 0)
+        self.assertEqual(tileSpec[0][2], 0)
+        self.assertEqual(tileSpec[0][3], 0)
+        self.assertEqual(str(tileSpec[0][0]), str(gagrid.tileBounds(0, 0, 0)))
+
+        gen = gagrid.iterGrid(13, 14)
+        tilesSpec = [i for i in gen]
+        self.assertEqual(len(tilesSpec), 12)
+        self.assertEqual(tilesSpec[0][1], 13)
+        self.assertEqual(tilesSpec[6][1], 14)
+        bounds = tilesSpec[2][0]
+        z = tilesSpec[2][1]
+        col = tilesSpec[2][2]
+        row = tilesSpec[2][3]
+        self.assertEqual(bounds, gagrid.tileBounds(z, col, row))
+
+        try:
+            for i in gagrid.iterGrid(13, 33):
+                print i
+        except Exception as e:
+            self.assertIsInstance(e, AssertionError)
+        else:
+            raise Exception('iterGrid: assertion error maxZoom too big not raised')
+
+        try:
+            for i in gagrid.iterGrid(-1, 11):
+                print i
+        except Exception as e:
+            self.assertIsInstance(e, AssertionError)
+        else:
+            raise Exception('iterGrid: assertion error minZoom too small not raised')
+
+        try:
+            for i in gagrid.iterGrid(13, 11):
+                print i
+        except Exception as e:
+            self.assertIsInstance(e, AssertionError)
+        else:
+            raise Exception('iterGrid: assertion error maxZoom bigger than minZoom not raised')
+
+    def testGetScale(self):
+        gagrid = GeoadminTileGrid()
+        s14 = gagrid.getScale(14)
+        s28 = gagrid.getScale(28)
+        self.assertGreater(s14, s28)
+        self.assertEqual(round(s14), 2456688.0)
+        self.assertEqual(round(s28), 378.0)
+
+    def testIterGridWithExtent(self):
+        offset = 20000.0
+        gagridDefault = GeoadminTileGrid()
+        extent = [gagridDefault.MINX + offset, gagridDefault.MINY + offset,
+                  gagridDefault.MAXX - offset, gagridDefault.MAXY - offset]
+        gagridExtent = GeoadminTileGrid(extent=extent)
+
+        self.assertGreater(gagridDefault.xSpan, gagridExtent.xSpan)
+        self.assertGreater(gagridDefault.ySpan, gagridExtent.ySpan)
+
+        tilesSpecDefault = [t for t in gagridDefault.iterGrid(20, 21)]
+        tilesSpecExtent = [t for t in gagridExtent.iterGrid(20, 21)]
+
+        self.assertGreater(len(tilesSpecDefault), len(tilesSpecExtent))
+        self.assertEqual(tilesSpecExtent[0][1], 20)
+        self.assertEqual(tilesSpecExtent[len(tilesSpecExtent) - 1][1], 21)
+
+    def testNumberOfTiles(self):
+        gagrid = GeoadminTileGrid()
+        nb = gagrid.numberOfTilesAtZoom(20)
+        self.assertEqual(nb, 23500)
+        nb = gagrid.numberOfTilesAtZoom(22)
+        self.assertEqual(nb, 375000)
