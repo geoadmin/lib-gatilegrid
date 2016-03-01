037b2d9993b43ffe38e56d641b7fde51000de4b5
loicgasser <loicgasser4@gmail.com>
2016-03-01 17:08:16 +0100

    Bump to 0.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='gatilegrid',
-      version='0.0.2',
+      version='0.0.3',
       description='Geoadmin custom tile grid for web mapping applications',
       classifiers=[],
       keywords='',
