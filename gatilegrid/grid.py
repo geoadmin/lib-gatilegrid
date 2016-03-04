a8f2e6210adad61077aa87d9099ec80cc98e95d7
loicgasser <loicgasser4@gmail.com>
2016-03-04 15:01:08 +0100

    Add grid module

---
+++ gatilegrid/grid.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+
+import math
+from past.builtins import xrange
+
+
+class Grid:
+    #   parameters:
+    #   extent [minX, minY, maxX, maxY]
+    #   resolutionX (signed resolution over x-axis)
+    #   resolutionY (signed resolution over y-axis)
+
+    #   case origin isTopLeft
+
+    #   a=(col=0,row=0)
+    #   b=(col=i,row=0)
+    #   c=(col=0,row=j)
+    #   d=(col=i,row=j)
+
+    #   a_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ b_    #
+    #   |_|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ |_|   #
+    #   |                                  |   #
+    #   |                                  |   #
+    #   |                                  |   #
+    #   |                                  |   #
+    #   c_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ d_|   #
+    #   |_|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ |_|   #
+
+    def __init__(self, extent, resolutionX, resolutionY):
+        self.extent = [float(e) for e in extent]
+        self.resolutionX = float(resolutionX)
+        self.resolutionY = float(resolutionY)
+        self._setOrigin()
+        self._setExtentAddress()
+
+    def __iter__(self):
+        for col in xrange(0, self.nbCellsX):
+            for row in xrange(0, self.nbCellsY):
+                cellExtent = self.cellExtent(col, row)
+                yield (cellExtent, col, row)
+
+    def cellExtent(self, col, row):
+        if self.isLeft:
+            minX = self.MINX + (col * self.resolutionX)
+            maxX = self.MINX + ((col + 1) * self.resolutionX)
+        elif self.isRight:
+            minX = self.MAXX + ((col + 1) * self.resolutionX)
+            maxX = self.MAXY + (col * self.resolutionX)
+
+        if self.isBottom:
+            minY = self.MINY + (row * self.resolutionY)
+            maxY = self.MINY + ((row + 1) * self.resolutionY)
+        elif self.isTop:
+            minY = self.MAXY + ((row + 1) * self.resolutionY)
+            maxY = self.MAXY + (row * self.resolutionY)
+
+        return [minX, minY, maxX, maxY]
+
+    def cellAddressFromPointCoordinate(self, pointCoordinate):
+        if not self.inExtent(pointCoordinate):
+            return None
+
+        [x, y] = pointCoordinate
+
+        if self.isLeft:
+            offsetX = x - self.MINX
+        elif self.isRight:
+            offsetX = self.MAXX - x
+
+        if self.isBottom:
+            offsetY = y - self.MINY
+        elif self.isTop:
+            offsetY = self.MAXY - y
+
+        col = abs(offsetX / self.resolutionX)
+        row = abs(offsetY / self.resolutionY)
+        if x in (self.MINX, self.MAXX) and col.is_integer():
+            col = max(0, col - 1)
+        if y in (self.MINY, self.MAXY) and row.is_integer():
+            row = max(0, row - 1)
+        return [
+            int(math.floor(col)),
+            int(math.floor(row))
+        ]
+
+    def inExtent(self, pointCoordinate):
+        [x, y] = pointCoordinate
+        return x >= self.MINX and x <= self.MAXX and \
+            y >= self.MINY and y <= self.MAXY
+
+    def _setOrigin(self):
+        if self.isBottomLeft:
+            self.origin = [self.extent[0], self.extent[1]]
+            self.end = [self.extent[2], self.extent[3]]
+        elif self.isBottomRight:
+            self.origin = [self.extent[2], self.extent[1]]
+            self.end = [self.extent[0], self.extent[3]]
+        elif self.isTopLeft:
+            self.origin = [self.extent[0], self.extent[3]]
+            self.end = [self.extent[2], self.extent[1]]
+        elif self.isTopRight:
+            self.origin = [self.extent[2], self.extent[3]]
+            self.end = [self.extent[0], self.extent[1]]
+
+    def _setExtentAddress(self):
+        [minCol, minRow] = self.cellAddressFromPointCoordinate(self.origin)
+        [maxCol, maxRow] = self.cellAddressFromPointCoordinate(self.end)
+        self.extentAddress = [minCol, minRow, maxCol, maxRow]
+
+    @property
+    def cellArea(self):
+        return abs(self.resolutionX * self.resolutionY)
+
+    @property
+    def nbCellsX(self):
+        [minCol, minRow, maxCol, maxRow] = self.extentAddress
+        return maxCol + 1
+
+    @property
+    def nbCellsY(self):
+        [minCol, minRow, maxCol, maxRow] = self.extentAddress
+        return maxRow + 1
+
+    @property
+    def nbCells(self):
+        return self.nbCellsX * self.nbCellsY
+
+    @property
+    def isLeft(self):
+        return self.resolutionX > 0
+
+    @property
+    def isRight(self):
+        return self.resolutionX < 0
+
+    @property
+    def isBottom(self):
+        return self.resolutionY > 0
+
+    @property
+    def isTop(self):
+        return self.resolutionY < 0
+
+    @property
+    def isTopLeft(self):
+        return self.isTop and self.isLeft
+
+    @property
+    def isBottomLeft(self):
+        return self.isBottom and self.isLeft
+
+    @property
+    def isTopRight(self):
+        return self.isTop and self.isRight
+
+    @property
+    def isBottomRight(self):
+        return self.isBottom and self.isRight
+
+    @property
+    def MINX(self):
+        return self.extent[0]
+
+    @property
+    def MAXX(self):
+        return self.extent[2]
+
+    @property
+    def MINY(self):
+        return self.extent[1]
+
+    @property
+    def MAXY(self):
+        return self.extent[3]
---
+++ tests/test_grid.py
@@ -0,0 +1,297 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from gatilegrid.grid import Grid
+
+
+class TestGeoadminTileGrid(unittest.TestCase):
+
+    def testGridBottomLeft(self):
+        extent = [minx, miny, maxx, maxy] = [0, 0, 100, 100]
+        resolutionX = 5
+        resolutionY = 5
+        grid = Grid(extent, resolutionX, resolutionY)
+
+        self.assertTrue(grid.isBottomLeft)
+        self.assertEqual(grid.cellArea, resolutionX * resolutionY)
+
+        col = 0
+        row = 0
+        [cminx, cminy, cmaxx, cmaxy] = grid.cellExtent(col, row)
+        self.assertEqual(cminx, minx)
+        self.assertEqual(cminy, miny)
+        self.assertEqual(cmaxx, resolutionX)
+        self.assertEqual(cmaxy, resolutionY)
+
+        col = 0
+        row = 1
+        [cminx, cminy, cmaxx, cmaxy] = grid.cellExtent(col, row)
+        self.assertEqual(cminx, minx)
+        self.assertEqual(cminy, resolutionY)
+        self.assertEqual(cmaxx, resolutionX)
+        self.assertEqual(cmaxy, 2 * resolutionY)
+
+        col = 1
+        row = 0
+        [cminx, cminy, cmaxx, cmaxy] = grid.cellExtent(col, row)
+        self.assertEqual(cminx, resolutionX)
+        self.assertEqual(cminy, miny)
+        self.assertEqual(cmaxx, 2 * resolutionX)
+        self.assertEqual(cmaxy, resolutionY)
+
+        nbCells = grid.nbCells
+        nbCellsX = grid.nbCellsX
+        nbCellsY = grid.nbCellsY
+        self.assertEqual(nbCellsX, (maxx - minx) / resolutionX)
+        self.assertEqual(nbCellsY, (maxy - miny) / resolutionY)
+        self.assertEqual(nbCells, nbCellsX * nbCellsY)
+
+        gridCellsSpec = [s for s in grid]
+        self.assertEqual(nbCells, len(gridCellsSpec))
+        col = 0
+        row = 0
+        self.assertEqual(gridCellsSpec[0][1], col)
+        self.assertEqual(gridCellsSpec[0][2], row)
+
+        [col, row] = grid.cellAddressFromPointCoordinate([0, 0])
+        self.assertEqual(col, 0)
+        self.assertEqual(row, 0)
+
+        [col, row] = grid.cellAddressFromPointCoordinate([1, 1])
+        self.assertEqual(col, 0)
+        self.assertEqual(row, 0)
+
+        [col, row] = grid.cellAddressFromPointCoordinate([5, 5])
+        self.assertEqual(col, 1)
+        self.assertEqual(row, 1)
+
+        [col, row] = grid.cellAddressFromPointCoordinate([7.5, 5])
+        self.assertEqual(col, 1)
+        self.assertEqual(row, 1)
+
+        [minCol, minRow, maxCol, maxRow] = grid.extentAddress
+        self.assertEqual(gridCellsSpec[nbCells - 1][1], maxCol)
+        self.assertEqual(gridCellsSpec[nbCells - 1][2], maxRow)
+
+        cellOutsideExtent = [200, 200]
+        cellAddress = grid.cellAddressFromPointCoordinate(cellOutsideExtent)
+        self.assertEqual(cellAddress, None)
+
+    def testGridTopLeft(self):
+        extent = [minx, miny, maxx, maxy] = [0, 0, 100, 100]
+        resolutionX = 5
+        resolutionY = -5
+        grid = Grid(extent, resolutionX, resolutionY)
+
+        self.assertTrue(grid.isTopLeft)
+        self.assertEqual(grid.cellArea, abs(resolutionX * resolutionY))
+
+        col = 0
+        row = 0
+        [cminx, cminy, cmaxx, cmaxy] = grid.cellExtent(col, row)
+        self.assertEqual(cminx, minx)
+        self.assertEqual(cminy, maxy + resolutionY)
+        self.assertEqual(cmaxx, resolutionX)
+        self.assertEqual(cmaxy, maxy)
+
+        col = 0
+        row = 1
+        [cminx, cminy, cmaxx, cmaxy] = grid.cellExtent(col, row)
+        self.assertEqual(cminx, minx)
+        self.assertEqual(cminy, maxy + (2 * resolutionY))
+        self.assertEqual(cmaxx, resolutionX)
+        self.assertEqual(cmaxy, maxy + resolutionY)
+
+        col = 1
+        row = 0
+        [cminx, cminy, cmaxx, cmaxy] = grid.cellExtent(col, row)
+        self.assertEqual(cminx, resolutionX)
+        self.assertEqual(cminy, maxy + resolutionY)
+        self.assertEqual(cmaxx, 2 * resolutionX)
+        self.assertEqual(cmaxy, maxy)
+
+        nbCells = grid.nbCells
+        nbCellsX = grid.nbCellsX
+        nbCellsY = grid.nbCellsY
+        self.assertEqual(nbCellsX, abs((maxx - minx) / resolutionX))
+        self.assertEqual(nbCellsY, abs((maxy - miny) / resolutionY))
+        self.assertEqual(nbCells, nbCellsX * nbCellsY)
+
+        gridCellsSpec = [s for s in grid]
+        self.assertEqual(nbCells, len(gridCellsSpec))
+        col = 0
+        row = 0
+        self.assertEqual(gridCellsSpec[0][1], col)
+        self.assertEqual(gridCellsSpec[0][2], row)
+
+        [col, row] = grid.cellAddressFromPointCoordinate([0, 100])
+        self.assertEqual(col, 0)
+        self.assertEqual(row, 0)
+
+        [col, row] = grid.cellAddressFromPointCoordinate([1, 99])
+        self.assertEqual(col, 0)
+        self.assertEqual(row, 0)
+
+        [col, row] = grid.cellAddressFromPointCoordinate([5, 95])
+        self.assertEqual(col, 1)
+        self.assertEqual(row, 1)
+
+        [col, row] = grid.cellAddressFromPointCoordinate([7.5, 95])
+        self.assertEqual(col, 1)
+        self.assertEqual(row, 1)
+
+        [minCol, minRow, maxCol, maxRow] = grid.extentAddress
+        self.assertEqual(gridCellsSpec[nbCells - 1][1], maxCol)
+        self.assertEqual(gridCellsSpec[nbCells - 1][2], maxRow)
+
+        cellOutsideExtent = [200, 200]
+        cellAddress = grid.cellAddressFromPointCoordinate(cellOutsideExtent)
+        self.assertEqual(cellAddress, None)
+
+    def testGridTopRight(self):
+        extent = [minx, miny, maxx, maxy] = [0, 0, 100, 100]
+        resolutionX = -5
+        resolutionY = -5
+        grid = Grid(extent, resolutionX, resolutionY)
+
+        self.assertTrue(grid.isTopRight)
+        self.assertEqual(grid.cellArea, abs(resolutionX * resolutionY))
+
+        col = 0
+        row = 0
+        [cminx, cminy, cmaxx, cmaxy] = grid.cellExtent(col, row)
+        self.assertEqual(cminx, maxx + resolutionX)
+        self.assertEqual(cminy, maxy + resolutionY)
+        self.assertEqual(cmaxx, maxx)
+        self.assertEqual(cmaxy, maxy)
+
+        col = 0
+        row = 1
+        [cminx, cminy, cmaxx, cmaxy] = grid.cellExtent(col, row)
+        self.assertEqual(cminx, maxx + resolutionX)
+        self.assertEqual(cminy, maxy + (2 * resolutionY))
+        self.assertEqual(cmaxx, maxx)
+        self.assertEqual(cmaxy, maxy + resolutionY)
+
+        col = 1
+        row = 0
+        [cminx, cminy, cmaxx, cmaxy] = grid.cellExtent(col, row)
+        self.assertEqual(cminx, maxx + (2 * resolutionX))
+        self.assertEqual(cminy, maxy + resolutionY)
+        self.assertEqual(cmaxx, maxx + resolutionX)
+        self.assertEqual(cmaxy, maxy)
+
+        nbCells = grid.nbCells
+        nbCellsX = grid.nbCellsX
+        nbCellsY = grid.nbCellsY
+        self.assertEqual(nbCellsX, abs((maxx - minx) / resolutionX))
+        self.assertEqual(nbCellsY, abs((maxy - miny) / resolutionY))
+        self.assertEqual(nbCells, nbCellsX * nbCellsY)
+
+        gridCellsSpec = [s for s in grid]
+        self.assertEqual(nbCells, len(gridCellsSpec))
+        col = 0
+        row = 0
+        self.assertEqual(gridCellsSpec[0][1], col)
+        self.assertEqual(gridCellsSpec[0][2], row)
+
+        maxCol = abs((maxx - minx) / resolutionX) - 1
+        [col, row] = grid.cellAddressFromPointCoordinate([100, 100])
+        self.assertEqual(col, 0)
+        self.assertEqual(row, 0)
+
+        [col, row] = grid.cellAddressFromPointCoordinate([0, 100])
+        self.assertEqual(col, maxCol)
+        self.assertEqual(row, 0)
+
+        [col, row] = grid.cellAddressFromPointCoordinate([1, 99])
+        self.assertEqual(col, maxCol)
+        self.assertEqual(row, 0)
+
+        [col, row] = grid.cellAddressFromPointCoordinate([5, 95])
+        self.assertEqual(col, maxCol)
+        self.assertEqual(row, 1)
+
+        [col, row] = grid.cellAddressFromPointCoordinate([7.5, 95])
+        self.assertEqual(col, maxCol - 1)
+        self.assertEqual(row, 1)
+
+        [minCol, minRow, maxCol, maxRow] = grid.extentAddress
+        self.assertEqual(gridCellsSpec[nbCells - 1][1], maxCol)
+        self.assertEqual(gridCellsSpec[nbCells - 1][2], maxRow)
+
+        cellOutsideExtent = [200, 200]
+        cellAddress = grid.cellAddressFromPointCoordinate(cellOutsideExtent)
+        self.assertEqual(cellAddress, None)
+
+    def testGridBottomRight(self):
+        extent = [minx, miny, maxx, maxy] = [0, 0, 100, 100]
+        resolutionX = -5
+        resolutionY = 5
+        grid = Grid(extent, resolutionX, resolutionY)
+
+        self.assertTrue(grid.isBottomRight)
+        self.assertEqual(grid.cellArea, abs(resolutionX * resolutionY))
+
+        col = 0
+        row = 0
+        [cminx, cminy, cmaxx, cmaxy] = grid.cellExtent(col, row)
+        self.assertEqual(cminx, maxx + resolutionX)
+        self.assertEqual(cminy, miny)
+        self.assertEqual(cmaxx, maxx)
+        self.assertEqual(cmaxy, miny + resolutionY)
+
+        col = 0
+        row = 1
+        [cminx, cminy, cmaxx, cmaxy] = grid.cellExtent(col, row)
+        self.assertEqual(cminx, maxx + resolutionX)
+        self.assertEqual(cminy, miny + resolutionY)
+        self.assertEqual(cmaxx, maxx)
+        self.assertEqual(cmaxy, miny + (2 * resolutionY))
+
+        col = 1
+        row = 0
+        [cminx, cminy, cmaxx, cmaxy] = grid.cellExtent(col, row)
+        self.assertEqual(cminx, maxx + (2 * resolutionX))
+        self.assertEqual(cminy, miny)
+        self.assertEqual(cmaxx, maxx + resolutionX)
+        self.assertEqual(cmaxy, miny + resolutionY)
+
+        nbCells = grid.nbCells
+        nbCellsX = grid.nbCellsX
+        nbCellsY = grid.nbCellsY
+        self.assertEqual(nbCellsX, abs((maxx - minx) / resolutionX))
+        self.assertEqual(nbCellsY, abs((maxy - miny) / resolutionY))
+        self.assertEqual(nbCells, nbCellsX * nbCellsY)
+
+        gridCellsSpec = [s for s in grid]
+        self.assertEqual(nbCells, len(gridCellsSpec))
+        col = 0
+        row = 0
+        self.assertEqual(gridCellsSpec[0][1], col)
+        self.assertEqual(gridCellsSpec[0][2], row)
+
+        [col, row] = grid.cellAddressFromPointCoordinate([100, 0])
+        self.assertEqual(col, 0)
+        self.assertEqual(row, 0)
+
+        [col, row] = grid.cellAddressFromPointCoordinate([99, 1])
+        self.assertEqual(col, 0)
+        self.assertEqual(row, 0)
+
+        [col, row] = grid.cellAddressFromPointCoordinate([94, 6])
+        self.assertEqual(col, 1)
+        self.assertEqual(row, 1)
+
+        maxCol = abs(((maxy - miny) / resolutionY)) - 1
+        [col, row] = grid.cellAddressFromPointCoordinate([100, 100])
+        self.assertEqual(col, 0)
+        self.assertEqual(row, maxCol)
+
+        [minCol, minRow, maxCol, maxRow] = grid.extentAddress
+        self.assertEqual(gridCellsSpec[nbCells - 1][1], maxCol)
+        self.assertEqual(gridCellsSpec[nbCells - 1][2], maxRow)
+
+        cellOutsideExtent = [200, 200]
+        cellAddress = grid.cellAddressFromPointCoordinate(cellOutsideExtent)
+        self.assertEqual(cellAddress, None)
