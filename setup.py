1745158682dd1fec2d86c2c6e06868bd839711e1
loicgasser <loicgasser4@gmail.com>
2017-04-20 18:32:56 +0200

    Bump to 0.0.9

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
 
 
 setup(name='gatilegrid',
-      version='0.0.8',
+      version='0.0.9',
       description='Popular tile grids and grids API for web mapping applications',
       classifiers=[],
       keywords='',
