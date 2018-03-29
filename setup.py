959ccfa6727845601c9ce50b0424f5b19d6094d0
Marcel Clausen <marcel.clausen@swisstopo.ch>
2018-03-29 11:11:03 +0000

    Bump to 0.1.10

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
 
 
 setup(name='gatilegrid',
-      version='0.1.9',
+      version='0.1.10',
       description='Popular tile grids and grids API for web mapping applications',
       classifiers=[],
       keywords='',
