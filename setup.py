75352916db7252505a6add7b531024b336795c63
Tobias Reber <tobias.reber@swisstopo.ch>
2023-05-25 09:26:35 +0200

    BGDIINF_SB-2908: bumping minor release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='gatilegrid',
-    version='0.2.1',
+    version='0.3.0',
     description='Popular tile grids and grids API for web mapping applications',
     keywords='gis wmts grid map',
     author='Loic Gasser',
